d902f9e2c109a9a98e3afa67851d91ca76746b9f
Akili Reid <71138517+AReid37@users.noreply.github.com>
2022-07-22 16:48:08 -0600

    Add project files.

---
+++ Dbug.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace HCResourceLibraryApp
+{
+    /// <summary>
+    ///     An intermediary, more focused version of the Debug Logger to Output
+    /// </summary>
+    class Dbug
+    {
+        /// Dbug.StartLogging()
+        /// Dbug.EndLogging()
+
+
+        public void LogLine()
+        {
+            Debug.WriteLine("message");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ConsoleFormat;
+
+namespace HCResourceLibraryApp
+{
+    // THE ENTRANCE POINT
+    class Program
+    {
+        static void Main()
+        {
+            Base.TextLine("Hello, High Contrast Resource Library App!");
+        }
+    }
+}
